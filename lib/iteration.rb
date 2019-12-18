9bd34195aa7a11c5d814bb4f54939bd23cc45af0
Sam Kyprianou <samuelkyprianou@gmail.com>
2019-12-18 12:33:07 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -36,7 +36,7 @@ row_index = 0
 while row_index < src.count do
   element_index = 0
   while element_index < src[row_index].count do
-    if src[row_index][element_index] && src[row_index]
+    if src[row_index][element_index]  && src[row_index]
     element_index += 1
   end
   row_index += 1
