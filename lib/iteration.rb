24e19818c1a1b3aee0f7b63f706250d8e3a7e831
Sam Kyprianou <samuelkyprianou@gmail.com>
2019-12-18 12:01:52 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -3,8 +3,8 @@ pizza_array = []
 row_index = 0
 while row_index < src.count do
   element_index = 0
-  while element_index < spice_rack[row_index].count do
-    puts spice_rack[row_index][element_index]
+  while element_index < src[row_index].count do
+    pizza_array << "I love src[row_index][element_index]
     element_index += 1
   end
   row_index += 1
